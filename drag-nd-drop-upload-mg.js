4695204deb9d50cf55df4bf4e9c8663a20ea8162
Abdelhadi HABCHI <abhabchi@sqli.com>
2019-04-23 21:48:57 +0100

    add dependencies

--- drag-nd-drop-upload-mg.js
+++ drag-nd-drop-upload-mg.js
@@ -1,5 +1,5 @@
 /*
- *  drag-nd-drop-upload-mg v1.0.0 - 2019
+ *  drag-nd-drop-upload-mg v1.0.4 - 2019
  *  Drag & Drop File Upload
  *  http://marocgeek.com
  *  (c) 2019 Abdelhadi Habchi - @adev
@@ -13,7 +13,6 @@ $(document).ready(function(){
   var message = "Faites glisser vos fichiers ici ou cliquez dans cette zone." ;
 
     // OnFileChange
-    // $('.drag-nd-drop-upload .upload-file').change(function () {
     $(document).on('change', '.drag-nd-drop-upload .upload-file', function(e) {
   
       if( this.files.length != 0 ){
@@ -25,7 +24,6 @@ $(document).ready(function(){
     });
 
     // OnUpload 
-    //1
     $(document).on('click', '.btn-upload-mg', function(e) {  
       $('.drag-nd-drop-upload p').text(message);
     });
